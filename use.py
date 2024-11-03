7e9022d793d3c7fa205b84681d3e5107e7e46641
Filin153 <dimons2006@yandex.ru>
2024-11-03 22:25:55 +0300

    update use.py

--- use.py
+++ use.py
@@ -1,12 +1,17 @@
 import requests
 
+IP = "IP"
+PORT = "PORT"
+# BASE_URL = "http://DOMAIN"
+BASE_URL = f"http://{IP}:{PORT}"
+
 def get_ai_answer(question: str, max_length: int, max_new_tokens: int):
     params = {
         'max_length': max_length,
         'max_new_tokens': max_new_tokens,
     }
 
-    response = requests.post('http://ip:port/llama', params=params, json=question, verify=False)
+    response = requests.post(f'{BASE_URL}/llama', params=params, json=question, verify=False)
 
     if response.status_code != 200:
         raise Exception(response.text)
@@ -18,7 +23,7 @@ def get_ai_answer_from_context(context: str, question: str):
         'question': question,
     }
 
-    response = requests.post('http://ip:port/context', params=params, json=context, verify=False)
+    response = requests.post(f'{BASE_URL}/context', params=params, json=context, verify=False)
 
     if response.status_code != 200:
         raise Exception(response.text)
